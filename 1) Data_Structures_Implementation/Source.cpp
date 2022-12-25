12843651897bb3a9a39992b9c7a6ee4973af9401
Mohamed-Y0usef <Mohamed.yussef777@hotmail.com>
2022-12-25 07:58:10 +0200

    First Commit

---
+++ 1) Data_Structures_Implementation/Source.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<stack>
+#include<queue>
+using namespace std;
+
+int main() {
+
+/*	int arr[5];
+
+	arr[0] = 10;
+	arr[1] = 20;
+	arr[3] = 30;
+
+	cout << arr[3] << endl; */
+
+/*    stack<int> stack;
+    stack.push(21);
+    stack.push(22);
+    stack.push(24);
+    stack.push(25);
+
+    cout << "Size: " << stack.size() << endl;
+    cout << "Top: " << stack.top() << endl;
+    stack.pop();
+    stack.push(70);
+    stack.push(80);
+    stack.pop();
+    cout << "Top: " << stack.top() << endl;
+    cout << "Empty(1) or not(0): " << stack.empty() << endl << endl;
+
+    while (!stack.empty()) {
+        cout << stack.top() << " ";
+        stack.pop();
+    }*/
+
+/*    queue<int> queue;
+
+    queue.push(21);
+    queue.push(22);
+    queue.push(24);
+    queue.push(25);
+
+    cout << "Size: " << queue.size() << endl;
+    cout << "Front: " << queue.front() << endl;
+    cout << "Back: " << queue.back() << endl;
+    queue.pop();
+    queue.push(70);
+    queue.push(80);
+    queue.pop();
+    cout << "Front: " << queue.front() << endl;
+    cout << "Back: " << queue.back() << endl;
+    cout << "Empty(1) or not(0): " << queue.empty() << endl << endl;
+
+    while (!queue.empty()) {
+        cout << queue.front() << " ";
+        queue.pop();
+    }*/
+
+/*    priority_queue<int> priority_queue;
+
+    priority_queue.push(21);
+    priority_queue.push(22);
+    priority_queue.push(24);
+    priority_queue.push(15);
+
+    cout << "Size: " << priority_queue.size() << endl;
+    cout << "Top: " << priority_queue.top() << endl;
+    priority_queue.pop();
+    priority_queue.push(70);
+    priority_queue.push(80);
+    priority_queue.pop();
+    cout << "Top: " << priority_queue.top() << endl;
+    cout << "Empty(1) or not(0): " << priority_queue.empty() << endl << endl;
+
+    while (!priority_queue.empty()) {
+        cout << priority_queue.top() << ", ";
+        priority_queue.pop();
+    }*/
+
+}
+
